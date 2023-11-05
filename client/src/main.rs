1e784b16238a3ae09d46cb944c2312b1a5324859
gabkhanfig <gabkhanfig@gmail.com>
2023-11-05 03:16:56 -0500

    Data Container For Jobs
    Sidesteps borrow checker using unsafe functions

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,54 @@
+use std::sync::{Arc, Mutex};
+
+use shared::engine::job::job_data::{JobFunc, JobRunDataBuffer, JobData};
+
+fn func(x: u32, y: u32, z: u32) {
+
+}
+
+struct Example {
+    num: u32
+}
+
+impl Example {
+    fn display_mut(&mut self) {
+        println!("{}", self.num);
+    }
+    fn display(&self) {
+        println!("{}", self.num);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut a = Example {num: 1};
+
+    println!("{}", std::mem::size_of::<JobFunc>());
+
+    //let job = unsafe { JobFunc::new_from_obj_mut::<Balls>(&mut a as *mut Balls, Balls::display) };
+    //let job2 = unsafe { JobFunc::new_from_obj_mut::<Balls>(&mut a as *mut Balls, Balls::display) };
+
+    let job1 = unsafe { JobData::from_obj_mut::<Example>(&mut a, Example::display_mut) };
+    let job2 = unsafe { JobData::from_obj::<Example>(&a, Example::display) };
+    unsafe {
+        a.num = 2;
+        job1.invoke();
+        a.num = 3;
+        job2.invoke();
+    }
+
+    //unsafe { job.invoke_member(JobRunDataBuffer::default()) };
+    //unsafe { job2.invoke_member(JobRunDataBuffer::default()) };
+
+    //let job = JobFunc::new( move || a.display() );
+    //job.func.as_mut()();
+
+    //let job2 = JobFunc::new( || a.display() );
+    //job.func.as_mut()();
+
+
+    //let bounded = || func(1, 2, 3);
+
+
 }
---
+++ shared/src/engine/job/mod.rs
@@ -0,0 +1,3 @@
+pub mod job_data;
+pub mod job_thread;
+pub mod job_system;
